324cda867331c564e515db0be1bfed4a0e7a96fe
Chidinma21 <chidinma@payourse.com>
2024-04-14 19:51:25 +0100

    setup basic project

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"github.com/Chidinma21/Price-Calculator/prices"
+)
+
 func main() {
-	
+	taxRates := []float64{0, 0.07, 0.1, 0.15}
+
+	for _, taxRate := range taxRates {
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
+	}
 }
